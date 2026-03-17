37d558ac6b91f0d3ca09420b72a7f0ef8cc95729
Andersen Cypriano <andersen.cypriano@gmail.com>
2026-03-17 17:11:56 -0300

    add sign-up button

--- src/app/(login)/sign-up/page.tsx
+++ src/app/(login)/sign-up/page.tsx
@@ -7,7 +7,7 @@ export default function SignUpPage() {
   const router = useRouter();
   const [error, setError] = useState<string | null>(null);
 
-  
+
 
   async function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -31,37 +31,37 @@ export default function SignUpPage() {
 
   return (
     <main className="max-w-md mx-auto p-6 space-y-4 text-white">
-      <h1 className="text-2xl font-bold">Sign Up</h1>
+      <h1 className="text-2xl font-bold text-black">Cadastrar Administrador</h1>
       {error && <p className="text-red-500">{error}</p>}
       <form onSubmit={handleSubmit} className="space-y-4">
-        
+
         <input
           name="name"
           placeholder="Nome Completo"
           required
-          className="w-full rounded-md bg-neutral-900 border border-neutral-700 px-3 py-2"
+          className="w-full rounded-md text-black border border-neutral-700 px-3 py-2"
         />
         <input
           name="email"
           type="email"
           placeholder="E-mail"
           required
-          className="w-full rounded-md bg-neutral-900 border border-neutral-700 px-3 py-2"
+          className="w-full rounded-md text-black border border-neutral-700 px-3 py-2"
         />
         <input
           name="password"
           type="password"
           placeholder="Password"
           required
-          minLength={8} 
-          className="w-full rounded-md bg-neutral-900 border border-neutral-700 px-3 py-2"
+          minLength={8}
+          className="w-full rounded-md text-black border border-neutral-700 px-3 py-2"
         />
         <button
           type="submit"
-          className="w-full bg-white text-black font-medium rounded-md px-4 py-2 hover:bg-gray-200"
+          className="w-full text-white bg-black font-medium rounded-md px-4 py-2 hover:bg-gray-200"
         >
 
-
+          Cadastrar
         </button>
       </form>
     </main>
